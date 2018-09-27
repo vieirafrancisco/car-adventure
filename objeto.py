e7707956798dbe93960f5bbd5fd87e3ed49bdfeb
Francisco <fvcneto.master98@gmail.com>
2018-09-27 09:06:37 -0300

    add get_image_surface method and others changes

--- objeto.py
+++ objeto.py
@@ -4,7 +4,6 @@ class Objeto():
 	def __init__(self, largura, comprimento, pos_x, pos_y, image_url = None):
 		if image_url:
 			self.image_surface = pygame.image.load(image_url)
-			
 
 		self.image_url = image_url
 		self.largura = largura
@@ -12,7 +11,11 @@ class Objeto():
 		self.pos_x = pos_x
 		self.pos_y = pos_y
 
+	def scale_image(self):
+		self.image_surface = pygame.transform.scale(self.image_surface, (self.largura, self.comprimento))
 
+	def draw(self, surface, cor):
+		pygame.draw.rect(surface, cor, self.get_pos()+self.get_size())
 
 	def get_pos(self):
 		return [self.pos_x, self.pos_y]
@@ -20,7 +23,5 @@ class Objeto():
 	def get_size(self):
 		return [self.largura, self.comprimento]
 
-
-
-	def draw(self, surface, cor):
-		pygame.draw.rect(surface, cor, self.get_pos()+self.get_size())
+	def get_image_surface(self):
+		return self.image_surface
