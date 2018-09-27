166cfac3c8d3fc34421fe476ddae87293e5ba0ad
Francisco <fvcneto.master98@gmail.com>
2018-09-27 19:45:08 -0300

    add a display object instance to KeyEvent class

--- key_events.py
+++ key_events.py
@@ -1,10 +1,13 @@
 class KeyEvent:
-    def __init__(self, player):
+    def __init__(self, display, player):
+        self._display = display
         self.player = player
         self.moviment = 40
 
     def key_left(self):
-        self.player.pos_x -= self.moviment
+        if self.player.pos_x - 40 >= 0:
+            self.player.pos_x -= self.moviment
 
     def key_right(self):
-        self.player.pos_x += self.moviment
+        if self.player.pos_x + 40 <= self._display.width-80:
+            self.player.pos_x += self.moviment
