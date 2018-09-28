30f20353c1fec9c635f8e1f8df26cc84df4c7b67
Francisco <fvcneto.master98@gmail.com>
2018-09-28 10:01:04 -0300

    change attributes names

--- objeto.py
+++ objeto.py
@@ -1,14 +1,14 @@
 import pygame
 
 class Objeto:
-	def __init__(self, largura, comprimento, pos_x, pos_y, image_url = None):
+	def __init__(self, width, height, pos_x, pos_y, image_url = None):
 		if image_url:
 			self.image_surface = pygame.image.load(image_url)
 
 		self.image_url = image_url
-		self.largura = largura
-		self.comprimento = comprimento
-		self.size = (self.largura, self.comprimento)
+		self.width = width
+		self.height = height
+		self.size = (self.width, self.height)
 		self.pos_x = pos_x
 		self.pos_y = pos_y
 
@@ -25,7 +25,7 @@ class Objeto:
 		return [self.pos_x, self.pos_y]
 
 	def get_size(self):
-		return [self.largura, self.comprimento]
+		return list(self.size)
 
 	def get_image_surface(self):
 		return self.image_surface
